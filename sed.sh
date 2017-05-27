e364e4e4aa418032cf1129041bde0b5f91b58639
pengttyy <pengttyy@gmail.com>
2017-05-26 22:45:45 -0700

    feat(字符串编辑): 使用sed将指定的字符串替换成目标字符串

--- sed.sh
+++ sed.sh
@@ -7,6 +7,7 @@ grep "^alias" ~/.bashrc | sed '2c 替换第二行'
 grep "^alias" ~/.bashrc | sed '2i 在第二行前插入\
     第三行'
 grep "^alias" ~/.bashrc | sed '2d'
+grep "^alias" ~/.bashrc | sed '2,3s/alias/object/g'
 
 
 
