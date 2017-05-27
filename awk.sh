984d5356d8447253367674beb46b8a27c509f29c
pengttyy <pengttyy@gmail.com>
2017-05-26 21:56:05 -0700

    feat(字符串处理): awk使用end在操作结束后执行操作

--- awk.sh
+++ awk.sh
@@ -3,3 +3,4 @@
 df -h | grep "/dev/sda" | awk '{print $5}' | cut -d "%" -f 1 
 
 awk 'BEGIN{FS=":"}{print $1 "\t" $3}' /etc/passwd 
+awk 'BEGIN{FS=":"}{print $1 "\t" $3}END{print "the END"}' /etc/passwd 
